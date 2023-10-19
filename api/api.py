e4c42b947b4dcba8cf2e59f8754e53b12be230c6
coskos-ops <afilkostic@gmail.com>
2023-10-19 05:23:31 -0400

    created login system

--- api/api.py
+++ api/api.py
@@ -7,6 +7,7 @@ from wtforms.validators import DataRequired, Email
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
 from flask_bootstrap import Bootstrap
+from wtforms import ValidationError
 
 # Set up a database file path
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -31,6 +32,8 @@ bootstrap = Bootstrap(app)
 # Model for user
 class User(db.Model):
     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    username = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
     firstname = db.Column(db.String(100), nullable=False)
     lastname = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(80), unique=True, nullable=False)
@@ -38,7 +41,9 @@ class User(db.Model):
     created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
     interests = db.Column(db.Text)
 
-    def __init__(self, firstname, lastname, email, phonenumber=None, interests=None):
+    def __init__(self, username, password, firstname, lastname, email, phonenumber=None, interests=None):
+        self.username = username
+        self.password = password
         self.firstname = firstname
         self.lastname = lastname
         self.email = email
@@ -49,44 +54,20 @@ class User(db.Model):
         return f"<Student {self.firstname}, {self.lastname}>"
 
 
-# Model for Event
-class Event(db.Model):
-    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    eventname = db.Column(db.String(100), nullable=False)
-    eventstart = db.Column(db.DateTime(timezone=True), nullable=False)
-    eventend = db.Column(db.DateTime(timezone=True), nullable=False)
-    eventlocation = db.Column(db.String(100), nullable=False)
-    oneliner = db.Column(db.String(80), nullable=False)
-    eventdesc = db.Column(db.Text)
-    eventimg = db.Column(db.Text)
-    eventimgtype = db.Column(db.Text)
 
-    def __init__(
-        self,
-        eventname,
-        eventstart,
-        eventend,
-        eventlocation,
-        oneliner,
-        eventdesc=None,
-        eventimg=None,
-        eventimgtype=None,
-    ):
-        self.eventname = eventname
-        self.eventstart = eventstart
-        self.eventend = eventend
-        self.eventlocation = eventlocation
-        self.oneliner = oneliner
-        self.eventdesc = eventdesc
-        self.eventimg = eventimg
-        self.eventimgtype = eventimgtype
-
-    def __repr__(self):
-        return f"<Event {self.eventname}>"
 
 
 # Temporary form - only for demonstrating the database. should be removed once login/registration is implemented
-class UserForm(FlaskForm):
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    password = StringField("Password", validators=[DataRequired()])
+
+
+    login = SubmitField("Login")
+
+class RegisterForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    password = StringField("Password", validators=[DataRequired()])
     firstname = StringField("What is your first name?", validators=[DataRequired()])
     lastname = StringField("What is your last name?", validators=[DataRequired()])
     email = StringField(
@@ -94,76 +75,63 @@ class UserForm(FlaskForm):
     )
     phone = StringField("What is your phone number?", validators=[DataRequired()])
     interests = StringField("What are your interests?")
-    submit = SubmitField("Submit")
+    register = SubmitField("Register")
 
 
-# Temporary form - only for demonstrating the database. should be removed once login/registration is implemented
-class EventForm(FlaskForm):
-    eventname = StringField("What is the Event Name?", validators=[DataRequired()])
-    eventstart = DateTimeField(
-        "When does the event start?", validators=[DataRequired()]
-    )
-    eventend = DateTimeField("When does the event end?", validators=[DataRequired()])
-    eventlocation = StringField(
-        "Where is the event located?", validators=[DataRequired()]
-    )
-    oneliner = StringField("Event One-Liner", validators=[DataRequired()])
-    eventdesc = StringField("What is the event description?")
-    submit = SubmitField("Submit")
 
 
 @app.route("/")
 def main():
     db.create_all()
-    return "Hello World"
+    return "go to /register or /login to try out the features of this branch"
 
 
-@app.route("/time")
-def get_current_time():
-    return {"time": time.time()}
 
 
-@app.route("/user", methods=["GET", "POST"])
-def user():
-    form = UserForm()
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    name = None
+    if form.validate_on_submit():
+        u = User.query.filter_by(username=form.username.data).all()
+        if len(u) == 1:
+            if u[0].password == form.password.data:
+                name = u[0].firstname
+            else:
+                name = "incorrect"
+        elif len(u) == 0:
+            name = "incorrect"
+        else:
+            raise Exception("Multiple users found in the database with the same username: " + str(u))
+
+    return render_template("login.html", form=form, name=name)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegisterForm()
     if form.validate_on_submit():
         user = User(
+            form.username.data,
+            form.password.data,
             form.firstname.data,
             form.lastname.data,
             form.email.data,
             form.phone.data,
             form.interests.data,
         )
-
         db.session.add(user)
         db.session.commit()
-        return redirect(url_for("user"))
-    return render_template("user.html", form=form)
+
+        return redirect(url_for("login"))
+    return render_template("register.html", form=form)
 
 
-@app.route("/event", methods=["GET", "POST"])
-def event():
-    form = EventForm()
-    if form.validate_on_submit():
-        event = Event(
-            form.eventname.data,
-            form.eventstart.data,
-            form.eventend.data,
-            form.eventlocation.data,
-            form.oneliner.data,
-            form.eventdesc.data,
-        )
-        db.session.add(event)
-        db.session.commit()
-        return redirect(url_for("event"))
-    return render_template("event.html", form=form)
 
 
-@app.route("/show_all")
-def show_all():
-    return render_template(
-        "show_all.html", users=User.query.all(), events=Event.query.all()
-    )
+
 
 
 # @app.errorhandler(404)
