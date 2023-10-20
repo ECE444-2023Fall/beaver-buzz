0a9ecbae5924bb00357ec0857c5ac4d094af7117
Filip Kostic <afilkostic@gmail.com>
2023-10-20 18:28:03 -0400

    Finished login and register

--- src/login.js
+++ src/login.js
@@ -1,68 +1,76 @@
 import React, {useState} from 'react'
 import {Form,Button} from 'react-bootstrap'
+import "./form.css"
 import {Grid} from "semantic-ui-react"
 import {Link} from "react-router-dom"
+import {useForm} from "react-hook-form";
 const LoginPage=()=>{
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    const[credentialsValid, setCredentials] = useState(true)
+    const[greeting, setGreeting] = useState('')
 
-
-
-    const handleLogin = () => {
-        console.log("Form submitted")
-        console.log(email)
-        console.log(password)
+     const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = (data) => {
+        console.log(data);
         const requestOptions={
-           method:"POST",
-           headers:{
-               'content-type':'application/json'
-           },
-           body:JSON.stringify({email:email, password:password})
-       }
-       fetch('/login', requestOptions)
+        method:"POST",
+        headers:{
+           'content-type':'application/json'
+        },
+        body:JSON.stringify(data)
+        }
+        fetch('/login', requestOptions)
         .then(response => response.json())
         .then(data => {
-            if(data.error) {
-                alert(data.error)
-            }
-            else if(data.greeting) {
-                alert(data.greeting)
-            }
-            console.log(data)
+        if(data.greeting) {
+            setCredentials(true);
+            setGreeting(data.greeting);
+        }
+        else {
+            setCredentials(false);
+            setGreeting('');
+        }
+        console.log(data)
         });
     }
+
+
+
 
 
     return(
         <div className ="container">
             <div className="form-center">
-                <Grid>
-                    <h1>Login</h1>
-                        <form>
-                            <label htmlFor="email">Email</label>
-                            <Form.Group>
-                                <Form.Control type="email"
-                                    placeholder="Email"
-                                    id="email"
-                                    onChange={(e)=>{setEmail(e.target.value)}}
-                                />
-                            </Form.Group>
-                            <br></br>
-                            <label htmlFor="password">Password</label>
-                            <Form.Group>
-                                <Form.Control type="password"
-                                    placeholder="Password"
-                                    id="password"
-                                    onChange={(e)=>{setPassword(e.target.value)}}
-                                />
-                            </Form.Group>
-                            <br></br>
-                            <Form.Group>
-                                <Button className="btn btn-primary" onClick={handleLogin}>Login</Button>
-                            </Form.Group>
-                        </form>
-                        <p>Don't have an account?</p><Link to='/register'>Register here</Link>
-                </Grid>
+
+                <h1>Login</h1><br/>
+                    <Form onSubmit={handleSubmit(onSubmit)}>
+                        <label htmlFor="email">Email</label>
+                        <Form.Group>
+                            <Form.Control type="email"
+                                placeholder="Email"
+                                id="input"
+                                          className="input"
+                                 {...register("email", { required: true})}
+                            />
+                        </Form.Group>
+                         {errors.email && <p className="error">{"Please enter your email"}</p>}
+                        <br></br>
+                        <label htmlFor="password">Password</label>
+                        <Form.Group>
+                            <Form.Control type="password"
+                                placeholder="Password"
+                                          className="input"
+                                id="input"
+                                 {...register("password", { required: true})}
+                            />
+                        </Form.Group>
+                         {errors.password && <p className="error">{"Please enter your password"}</p>}
+                        <br></br>
+                        {credentialsValid ? <p>{greeting}</p> : <p className="error">Invalid email or password</p>}
+                        <Button type='submit' className="submitButton">Login</Button>
+
+                    </Form>
+                    <p>Don't have an account?</p><Link to='/register'>Register here</Link>
+
             </div>
 
         </div>
--- src/register.js
+++ src/register.js
@@ -1,116 +1,124 @@
 import React, {useState} from 'react'
 import {Form,Button} from 'react-bootstrap'
-import {Grid} from "semantic-ui-react"
+import "./form.css"
+import {useForm} from "react-hook-form";
+import {useNavigate} from "react-router-dom";
 const RegisterPage=()=>{
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [firstname, setFirstname] = useState('')
-    const [lastname, setLastname] = useState('')
-    const [phonenumber, setPhonenumber] = useState('')
-    const [interests, setInterests] = useState('')
+    const[emailTaken, setTaken] = useState(false)
+    const[greeting, setGreeting] = useState('')
 
+    const navigate = useNavigate();
 
-
-    const handleRegister = () => {
-        console.log("Form submitted")
-        console.log(email)
-        console.log(password)
+     const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = (data) => {
+        console.log(data);
         const requestOptions={
-           method:"POST",
-           headers:{
-               'content-type':'application/json'
-           },
-           body:JSON.stringify({email:email, password:password})
-       }
-       fetch('/register', requestOptions)
+        method:"POST",
+        headers:{
+           'content-type':'application/json'
+        },
+        body:JSON.stringify(data)
+        }
+        fetch('/register', requestOptions)
         .then(response => response.json())
         .then(data => {
-            if(data.error) {
-                alert(data.error)
-            }
-            else if(data.greeting) {
-                alert(data.greeting)
-            }
-            console.log(data)
+        if(data.greeting) {
+            setTaken(false);
+            setGreeting(data.greeting);
+            navigate("/login");
+        }
+        else {
+            setTaken(true);
+            setGreeting('');
+        }
+        console.log(data)
         });
     }
+
+
+    
 
 
     return(
         <div className ="container">
             <div className="form-center">
-                <Grid>
-                    <h1>Register</h1>
-                        <form>
-                            <label htmlFor="email">Email</label>
-                            <Form.Group>
-                                <Form.Control type="email"
-                                    placeholder="Email"
-                                    id="email"
-                                    onChange={(e)=>{setEmail(e.target.value)}}
-                                />
-                            </Form.Group>
-                            <br></br>
-                            <label htmlFor="password">Password</label>
-                            <Form.Group>
-                                <Form.Control type="password"
-                                    placeholder="Password"
-                                    id="password"
-                                    onChange={(e)=>{setPassword(e.target.value)}}
-                                />
-                            </Form.Group>
-                            <br></br>
-                            <label htmlFor="firstname">First name</label>
+                <h1>Register</h1><br/>
+                    <Form onSubmit={handleSubmit(onSubmit)}>
+                        <label htmlFor="email">Email</label>
+                        <Form.Group>
+                            <Form.Control type="email"
+                                placeholder="Email"
+                                id="input"
+                                          className="input"
+                                 {...register("email", { required: true, maxLength: 100 })}o
+                            />
+                        </Form.Group>
+                         {errors.email && <p className="error">{"Please enter a valid email address"}</p>}
+                        <br></br>
+                        <label htmlFor="password">Password</label>
+                        <Form.Group>
+                            <Form.Control type="password"
+                                placeholder="Password"
+                                          className="input"
+                                id="input"
+                                 {...register("password", { required: true, minLength: 8, maxLength: 100 })}
+                            />
+                        </Form.Group>
+                        {errors.password && <p className="error">{"Please enter your password (8-100 chars)"}</p>}
+                        <br/>
+                         <label htmlFor="firstname">First name</label>
                             <Form.Group>
                                 <Form.Control type="text"
                                     placeholder="First name"
                                     id="firstname"
-                                    onChange={(e)=>{setFirstname(e.target.value)}}
+                                               className="input"
+                                    {...register("firstname", { required: true})}
                                 />
                             </Form.Group>
+                           {errors.firstname && <p className="error">{"Please enter your first name"}</p>}
                             <br></br>
 
                             <label htmlFor="lastname">Last name</label>
                             <Form.Group>
                                 <Form.Control type="text"
                                     placeholder="Last name"
+                                               className="input"
                                     id="lastname"
-                                    onChange={(e)=>{setLastname(e.target.value)}}
+                                    {...register("lastname", { required: true})}
                                 />
                             </Form.Group>
+                           {errors.lastname && <p className="error">{"Please enter your last name"}</p>}
                             <br></br>
 
                             <label htmlFor="phonenumber">Phone number (Optional)</label>
                             <Form.Group>
                                 <Form.Control type="tel"
-                                    pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
                                     placeholder="Phone number"
+                                   pattern = "[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                                   className="input"
                                     id="phonenumber"
-                                    onChange={(e)=>{setPhonenumber(e.target.value)}}
+                                     {...register("phonenumber", { required: false})}
                                 />
                             </Form.Group>
                             <br></br>
 
-                            <label htmlFor="interests">What are your interests?</label>
+                            <label htmlFor="interests">What are your interests? (Optional)</label>
                             <Form.Group>
                                 <Form.Control as="textarea"
                                         rows={3}
                                         placeholder="Tell us about your interests!"
                                         id="interests"
+                                               className="input"
                                               resize = "none"
                                               width = "100px"
-                                        onChange={(e)=>{setInterests(e.target.value)}}
+                                        {...register("interests", { required: false})}
                                 />
-
                             </Form.Group>
-                            <br></br>
+                        {!emailTaken ? <p>{greeting}</p> : <p className="error">There is already an account registered with this email</p>}
+                        <Button type='submit' className="submitButton">Register</Button>
 
+                    </Form>
 
-                            <Form.Group>
-                                <Button className="btn btn-primary" onClick={handleRegister}>Register</Button>
-                            </Form.Group>
-                        </form>
-                </Grid>
             </div>
 
         </div>
