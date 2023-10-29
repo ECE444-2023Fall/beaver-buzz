e3205722e8f8b1155d2f2cb36fe92e4a14ec3148
Steven Zhang <stevezzhang@gmail.com>
2023-10-29 12:16:13 -0400

    Updated database configuration to Postgres on Heroku

--- api/Configuration.py
+++ api/Configuration.py
@@ -1,5 +1,4 @@
 class Configuration:
     SECRET_KEY = "ry4nch3n"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = r"sqlite:///./db.sqlite"
-
+    SQLALCHEMY_DATABASE_URI = "postgres://oeypxzjtmdwmso:fd700d3a605375df5d8c178763efe9a32b423aa4643a2360ed608a870bd2a753@ec2-34-193-110-25.compute-1.amazonaws.com:5432/dbj8go8nj9fmjj"
