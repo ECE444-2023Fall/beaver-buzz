9b4755d3dffdb748bd78c967ccac7eae34cf1144
Steven Zhang <stevezzhang@gmail.com>
2023-10-29 16:47:38 -0400

    Set uri as environment variable

--- api/Configuration.py
+++ api/Configuration.py
@@ -1,8 +1,9 @@
+import os
 import re
 
 class Configuration:
     SECRET_KEY = "ry4nch3n"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    original_uri = "postgres://zsitdxsagpsldt:16d14b5c5bdabc1b38c5b5d435757f84f7afcf0bf1f5d93db76f34b069e6a77c@ec2-34-193-110-25.compute-1.amazonaws.com:5432/df4cpci37ne0rp"
+    original_uri = os.environ.get('SQLALCHEMY_DATABASE_URI', 'fallback_url_here')
     corrected_uri = re.sub(r'^postgres:', 'postgresql:', original_uri)
     SQLALCHEMY_DATABASE_URI = corrected_uri
