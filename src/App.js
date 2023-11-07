e59b35694b24eaa3b6a7ad4a3585b95d510d211f
Filip Kostic <afilkostic@gmail.com>
2023-11-07 14:22:18 -0500

    Created generic user page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import EventPage from './Event';
 import DiscoverPage from "./discover";
 import {BrowserRouter as Router, Routes, Route, BrowserRouter} from 'react-router-dom';
 import { UserProvider } from './UserContext';
-import UserPage from "./userProfile";
+import ProfilePage from "./Profile";
+import UserPage from './UserProfile';
 
 function App() {
 
@@ -20,9 +21,10 @@ function App() {
                 <Routes>
                     <Route exact path='/discover' element={<DiscoverPage/>}/>
                     <Route exact path='/login' element={<LoginPage/>}/>
-                    <Route exact path='/profile' element={<UserPage/>}/>
+                    <Route exact path='/profile' element={<ProfilePage/>}/>
                     <Route exact path='/register' element={<RegisterPage/>}/>
                     <Route exact path='/events' element={<EventPage/>}/>
+                    <Route exact path='/user/:id' element={<UserPage/>}/>
                 </Routes>
             </Router>
         </UserProvider>
