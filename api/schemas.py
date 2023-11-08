d7f46b1f94f01b566e6fe3e0e2b637442bcf3feb
Himanish Jindal <jhimanish@gmail.com>
2023-11-08 18:58:31 -0500

    Updating database with new fields and tables

--- api/schemas.py
+++ api/schemas.py
@@ -12,6 +12,14 @@ event_attendance = db.Table(
     db.Column("eventID", db.Integer, db.ForeignKey("events.id")),
 )
 
+# Tracks who a user is subscribed to
+# Many-to-many relationship
+user_subscribed_to = db.Table(
+    "user_subscribed_to",
+    db.Column("userID", db.Integer, db.ForeignKey("users.id")),
+    db.Column("subscriberID", db.Integer, db.ForeignKey("users.id")),
+)
+
 
 # Database Schema for User model
 class User(db.Model):
@@ -24,15 +32,29 @@ class User(db.Model):
     phonenumber = db.Column(db.Unicode(20))
     created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
     interests = db.Column(db.Text)
+    userImg = db.Column(db.Text)
+    userImgType = db.Column(db.Text)
     # events that this person has organized (1 to many relationship)
     organizedEvents = db.relationship("Event", backref="user")
     # events that this person is attending
     registeredEvents = db.relationship(
         "Event", secondary=event_attendance, backref="users"
     )
+    # users that this person is subscribed to
+    subscribed_to_users = db.relationship(
+        "User", secondary=user_subscribed_to, backref="subscribers"
+    )
 
     def __init__(
-        self, password, firstname, lastname, email, phonenumber=None, interests=None
+        self,
+        password,
+        firstname,
+        lastname,
+        email,
+        phonenumber=None,
+        interests=None,
+        userImg=None,
+        userImgType=None,
     ):
         self.password = password
         self.firstname = firstname
@@ -40,6 +62,8 @@ class User(db.Model):
         self.email = email
         self.phonenumber = phonenumber
         self.interests = interests
+        self.userImg = userImg
+        self.userImgType = userImgType
 
     def __repr__(self):
         return f"<Student {self.firstname}, {self.lastname}>"
@@ -49,6 +73,7 @@ class User(db.Model):
 
     def serialize(self):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
 
 # Database Schema for Event Model
 class Event(db.Model):
@@ -64,7 +89,7 @@ class Event(db.Model):
     eventDesc = db.Column(db.Text)
     eventImg = db.Column(db.Text)
     eventImgType = db.Column(db.Text)
-    # eventUsers = db.relationship(db.Integer, secondary=event_attendance)
+    eventCategories = db.Column(db.Text)
     registered = db.Column(db.Integer, default=0)
 
     def __init__(
@@ -79,6 +104,7 @@ class Event(db.Model):
         eventDesc=None,
         eventImg=None,
         eventImgType=None,
+        eventCategories=None,
     ):
         self.eventName = eventName
         self.organizerID = organizerID
@@ -90,8 +116,10 @@ class Event(db.Model):
         self.eventDesc = eventDesc
         self.eventImg = eventImg
         self.eventImgType = eventImgType
+        self.eventCategories = eventCategories
 
     def __repr__(self):
         return f"<Event {self.eventName}>"
+
     def serialize(self):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
