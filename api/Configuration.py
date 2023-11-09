6f48d8ec41a9c24532947e73f5266a0e262a845e
Himanish Jindal <jhimanish@gmail.com>
2023-11-08 19:10:14 -0500

    Fixing backend by removing postgres changes

--- api/Configuration.py
+++ api/Configuration.py
@@ -1,9 +1,7 @@
 import os
-import re
+
 
 class Configuration:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = "ry4nch3n"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    original_uri = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    corrected_uri = re.sub(r'^postgres:', 'postgresql:', original_uri)
-    SQLALCHEMY_DATABASE_URI = corrected_uri
+    SQLALCHEMY_DATABASE_URI = r"sqlite:///./db.sqlite"
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,67 +3,67 @@ import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Button } from './Button';
 import './Navbar.css';
-import {useUserContext} from "../UserContext";
+import { useUserContext } from "../UserContext";
 
 function Navbar() {
     const [click, setClick] = useState(false);
     const [button, setButton] = useState(true);
 
-  const  {
+    const {
         userId,
         setUserId
     } = useUserContext()
 
 
-  const handleClick = () => setClick(!click);
-  const closeMobileMenu = () => setClick(false);
-  const showButton = () => {
-    if (window.innerWidth <= 960){ setButton(false);}
-    else { setButton(true);}
-  };
-  useEffect(()=> {showButton();}, []);
-  window.addEventListener('resize', showButton);
-  return (
-    <>
-        <nav className='navbar'>
-            <div className='navbar-container'>
-                <Link to="/" className='navbar-logo' onClick={closeMobileMenu}>
-                    BeaverBuzz
-                </Link>
-                <div className='menu-icon'onClick={handleClick}>
-                    <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
-                </div>
-                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                    <li className='nav-item'>
-                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-                            Home
-                        </Link>
-                    </li>
-                    <li className='nav-item'>
-                        <Link to='/events' className='nav-links' onClick={closeMobileMenu}>
-                            Events
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to='/discover' className='nav-links' onClick={closeMobileMenu}>
-                            Discover
-                        </Link>
-                    </li>
-                    <li className='nav-item'>
-                        {!userId ? <Link to='/login' className='nav-links' onClick={closeMobileMenu}>
-                            Login
-                        </Link> : <Link className='nav-links' onClick={() =>{closeMobileMenu(); setUserId(null); localStorage.setItem('user', null)}} to={'/'}>
-                            Logout
-                        </Link>}
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+    const showButton = () => {
+        if (window.innerWidth <= 960) { setButton(false); }
+        else { setButton(true); }
+    };
+    useEffect(() => { showButton(); }, []);
+    window.addEventListener('resize', showButton);
+    return (
+        <>
+            <nav className='navbar'>
+                <div className='navbar-container'>
+                    <Link to="/" className='navbar-logo' onClick={closeMobileMenu}>
+                        BeaverBuzz
+                    </Link>
+                    <div className='menu-icon' onClick={handleClick}>
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
+                    </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className='nav-item'>
+                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/post-event' className='nav-links' onClick={closeMobileMenu}>
+                                Post Event
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='/profile' className='nav-links' onClick={closeMobileMenu}>
+                                Profile
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            {!userId ? <Link to='/login' className='nav-links' onClick={closeMobileMenu}>
+                                Login
+                            </Link> : <Link className='nav-links' onClick={() => { closeMobileMenu(); setUserId(null); localStorage.setItem('user', null) }} to={'/'}>
+                                Logout
+                            </Link>}
 
-                    </li>
-                    
-                </ul>
-                {button && !userId && <Button buttonStyle='btn--outline' linkTo='/register'>SIGN UP</Button>}
-            </div>
-        </nav>
-    </>
-  )
+                        </li>
+
+                    </ul>
+                    {button && !userId && <Button buttonStyle='btn--outline' linkTo='/register'>SIGN UP</Button>}
+                </div>
+            </nav>
+        </>
+    )
 }
 
 export default Navbar
