261b203bead27ab88adee6c8f97fa4de177f2d02
Steven Zhang <stevezzhang@gmail.com>
2023-11-12 16:34:41 -0500

    Fixed database initialization and SQLAlchemy URI

--- api/Configuration.py
+++ api/Configuration.py
@@ -1,6 +1,10 @@
 import os
 
+uri = os.getenv("DATABASE_URL")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
 class Configuration:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'default-secret-key')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or "sqlite:///./db.sqlite"
+    SQLALCHEMY_DATABASE_URI = uri
