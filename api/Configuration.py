1dfa2168a357738e170ce73b20b601467c70f82f
Steven Zhang <stevezzhang@gmail.com>
2023-11-12 16:26:15 -0500

    Update database configuration

--- api/Configuration.py
+++ api/Configuration.py
@@ -1,7 +1,6 @@
 import os
 
 class Configuration:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "default-secret-key")
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'default-secret-key')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # Fallback to a local sqlite database if no DATABASE_URL is set
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///./db.sqlite").replace("postgres://", "postgresql://", 1)
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or "sqlite:///./db.sqlite"
