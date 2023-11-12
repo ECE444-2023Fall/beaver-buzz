746f0f5d9df7773c2fee1975253f5b62f1e8d95b
Steven Zhang <stevezzhang@gmail.com>
2023-11-12 16:46:15 -0500

    Add debugging line

--- api/Configuration.py
+++ api/Configuration.py
@@ -1,8 +1,10 @@
 import os
 
-uri = os.getenv("DATABASE_URL")
-if uri.startswith("postgres://"):
+uri = os.getenv('DATABASE_URL')
+print("Database URI:", uri)  # Debugging line to be removed later
+if uri and uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
+
 
 class Configuration:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'default-secret-key')
