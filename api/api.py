0f528926fe5944647eb5914e69191306c5f20b8f
Steven Zhang <stevezzhang@gmail.com>
2023-11-12 18:29:28 -0500

    Modify string to bytes

--- api/api.py
+++ api/api.py
@@ -92,14 +92,14 @@ def login():
     email = request.json["email"]
     password = request.json["password"]
 
-    print(type(password.encode("utf-8")))
+#     print(type(password.encode("utf-8")))
 
     user = User.query.filter_by(email=email).first()
 
-    print(type(user.password))
-    print(user.password + '\n' + password.encode("utf-8"))
+#     print(type(user.password))
+#     print(user.password + '\n' + password.encode("utf-8"))
 
-    if user is None or not bcrypt.checkpw(password.encode("utf-8"), user.password):
+    if user is None or not bcrypt.checkpw(password.encode("utf-8"), bytes(user.password, 'utf-8')):
         return jsonify({"error": "Invalid username or password"}), 401
 
     return jsonify({
