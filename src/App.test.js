cdcc99bb490520db7ec276f6f733b9c855568e32
Himanish Jindal <jhimanish@gmail.com>
2023-11-12 17:24:52 -0500

    Fixing frontend render test to check for BeaverBuzz (name of app)

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders beaverbuzz on navbar', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText("BeaverBuzz");
   expect(linkElement).toBeInTheDocument();
 });
