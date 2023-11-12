e4632218c1d0352e07e30cf20003eb99ee6a4e53
Steven Zhang <stevezzhang@gmail.com>
2023-11-12 10:21:20 -0500

    Convert secret keys and Heroku database URL to environment variables

--- api/Configuration.py
+++ api/Configuration.py
@@ -1,7 +1,7 @@
 import os
 
-
 class Configuration:
-    SECRET_KEY = "ry4nch3n"
+    SECRET_KEY = os.environ.get("SECRET_KEY", "default-secret-key")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = r"sqlite:///./db.sqlite"
+    # Fallback to a local sqlite database if no DATABASE_URL is set
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///./db.sqlite").replace("postgres://", "postgresql://", 1)
