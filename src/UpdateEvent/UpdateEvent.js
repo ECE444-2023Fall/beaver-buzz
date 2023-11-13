dc26ff2e59bb2eb15cd304058e3bde94e99db371
Himanish Jindal <jhimanish@gmail.com>
2023-11-13 01:11:00 -0500

    Updating event page so that page alerts user when event has been successfully updated

--- src/UpdateEvent/UpdateEvent.js
+++ src/UpdateEvent/UpdateEvent.js
@@ -25,6 +25,15 @@ export default function EventUpdatePage() {
     const navigate = useNavigate();
     const { userId } = useContext(UserContext);
     const [data, setData] = useState([]);
+    const [visibility, setVisibility] = useState(false)
+    const [eventName, setEventName] = useState([])
+    const [oneLiner, setOneLiner] = useState([])
+    const [eventDate, setEventDate] = useState([])
+    const [startTime, setStartTime] = useState([])
+    const [endTime, setEndTime] = useState([])
+    const [eventBuilding, setEventBuilding] = useState([])
+    const [eventRoom, setEventRoom] = useState([])
+    const [eventDesc, setEventDesc] = useState([])
 
     useEffect(() => {
         const fetchInfo = () => {
@@ -54,6 +63,14 @@ export default function EventUpdatePage() {
                     d["startTime"] = startTime.substring(0, 5)
                     d["endTime"] = endTime.substring(0, 5)
                     setTags(d["eventCategories"])
+                    setEventName(d["eventName"])
+                    setOneLiner(d["oneLiner"])
+                    setEventDate(d["eventDate"])
+                    setStartTime(d["startTime"])
+                    setEndTime(d["endTime"])
+                    setEventBuilding(d["eventBuilding"])
+                    setEventRoom(d["eventRoom"])
+                    setEventDesc(d["eventDesc"])
                     setData(d)
                 })
                 .catch((error) => { console.log(error); setData(-1); })
@@ -63,6 +80,14 @@ export default function EventUpdatePage() {
 
     const onSubmit = async (data) => {
         console.log(data);
+        data['eventName'] = eventName;
+        data['oneLiner'] = oneLiner;
+        data['eventDate'] = eventDate
+        data['eventStart'] = startTime
+        data['eventEnd'] = endTime
+        data['building'] = eventBuilding
+        data['room'] = eventRoom
+        data['description'] = eventDesc
         data['organizerID'] = userId;
         data['image'] = data['image'].length == 0 ? false : await getBase64(data['image'].item(0));
         data['eventCategories'] = selectedTags;
@@ -79,7 +104,11 @@ export default function EventUpdatePage() {
             .then(response => response.json())
             .then(data => {
                 if (data['event_id']) {
-                    navigate("/events/" + data.event_id);
+                    setVisibility(true)
+                    setTimeout(() => {
+                        setVisibility(false);
+                        navigate("/events/" + data.event_id);
+                    }, 5000);
                 }
                 console.log(data)
             });
@@ -89,48 +118,56 @@ export default function EventUpdatePage() {
     return (
         <div>
             <div className="host-container">
-                <h1>Update Event</h1>
+                {visibility && <div className="alert-container">
+                    <div className="alert alert-info">
+                        <span className="close">X</span>
+                        <p>Event has been successfully updated!</p>
+                        <p>Routing to event page in 5 seconds</p>
+                    </div>
+                </div>
+                }
                 <Form onSubmit={handleSubmit(onSubmit)} className='host-form'>
+                    <h1>Update Event</h1>
                     <Form.Group controlId="eventName">
                         <Form.Label>Event Name</Form.Label>
-                        <Form.Control type="text" defaultValue={data.eventName} maxLength={50}{...register("eventName", { required: true })} />
+                        <Form.Control type="text" defaultValue={eventName} onChange={(e) => setEventName(e.target.value)} maxLength={50} />
                     </Form.Group>
 
                     <Form.Group controlId="oneLiner">
                         <Form.Label>One-liner</Form.Label>
-                        <Form.Control type="text" defaultValue={data.oneLiner} maxLength={75} {...register("oneLiner", { required: true })} />
+                        <Form.Control type="text" defaultValue={oneLiner} onChange={(e) => setOneLiner(e.target.value)} maxLength={75} />
                     </Form.Group>
 
                     <Form.Group controlId="eventDate">
                         <Form.Label>Date</Form.Label>
-                        <Form.Control type="date" defaultValue={data.eventDate} {...register("eventDate", { required: true })} />
+                        <Form.Control type="date" defaultValue={eventDate} onChange={(e) => setEventDate(e.target.value)} />
                     </Form.Group>
                     <div className="form-row">
                         <div className="col">
                             <Form.Group controlId="eventStart">
                                 <Form.Label>Start Time</Form.Label>
-                                <Form.Control type="time" defaultValue={data.startTime} {...register("eventStart", { required: true })} />
+                                <Form.Control type="time" defaultValue={startTime} onChange={(e) => setStartTime(e.target.value)} />
                             </Form.Group></div>
                         <div className="col">
                             <Form.Group controlId="eventEnd">
                                 <Form.Label>End Time</Form.Label>
-                                <Form.Control type="time" defaultValue={data.endTime} {...register("eventEnd", { required: true })} />
+                                <Form.Control type="time" defaultValue={endTime} onChange={(e) => setEndTime(e.target.value)} />
                             </Form.Group></div>
                     </div>
 
                     <Form.Group controlId="building">
                         <Form.Label>Building</Form.Label>
-                        <Form.Control type="text" defaultValue={data.eventBuilding} maxLength={25} {...register("building", { required: true })} />
+                        <Form.Control type="text" defaultValue={eventBuilding} onChange={(e) => setEventBuilding(e.target.value)} />
                     </Form.Group>
 
                     <Form.Group controlId="room">
                         <Form.Label>Room</Form.Label>
-                        <Form.Control type="text" defaultValue={data.eventRoom} maxLength={10} {...register("room", { required: true })} />
+                        <Form.Control type="text" defaultValue={eventRoom} onChange={(e) => setEventRoom(e.target.value)} />
                     </Form.Group>
 
                     <Form.Group controlId="description">
                         <Form.Label>Description</Form.Label>
-                        <Form.Control as="textarea" rows={3} maxLength={256} defaultValue={data.eventDesc} {...register("description", { required: true })} />
+                        <Form.Control as="textarea" rows={3} maxLength={256} defaultValue={eventDesc} onChange={(e) => setEventDesc(e.target.value)} />
                     </Form.Group>
 
                     <Form.Group controlId="image">
