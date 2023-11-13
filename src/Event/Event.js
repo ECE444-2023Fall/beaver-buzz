6bb429472461d872f91225d45c84b613f1812364
coskos-ops <afilkostic@gmail.com>
2023-11-13 13:06:51 -0500

    Fix link

--- src/Event/Event.js
+++ src/Event/Event.js
@@ -151,7 +151,7 @@ export default function EventPage() {
                     <head>
                         <title>{data.eventName}</title>
                     </head>
-                    <body>
+                    <body>s
                         <div id="eventContainer">
                             {eventOwner &&
                                 <div className="editButtonContainer">
@@ -166,7 +166,7 @@ export default function EventPage() {
                             <img id="eventImage" src={data.eventImg} alt="Event"></img>
                             <p id="eventDescription">{data.eventDesc}</p>
                             <div id="eventInfo">
-                                <p><strong>Organizer: </strong><Link onClick={event => {navigate('/user/' + data.organizerID);}}>{data.organizerName}</Link></p>
+                                <p><strong>Organizer: </strong><Link to = {'/user/' + data.organizerID}>{data.organizerName}</Link></p>
                                 <p><strong>Date and Time: </strong>{convertDate(data.eventStart)}</p>
                                 <p><strong>Location: </strong>{data.eventBuilding}, Room {data.eventRoom}</p>
                                 {data.eventCategories && <p><strong>Event Categories: </strong>{
